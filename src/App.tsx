4f8e2ef3422ffbb253f215d9f44c8ddf91bb3b2f
Collins Anele <collinsanele@gmail.com>
2021-08-27 07:20:54 -0700

    added svg functionality in declarations.d.ts and also in webpack.config.js

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 import './styles.css'
+import IMAGE from './display.png'
 
 export const App = ()=>{
-    return <h1>React Typescript Webpack Starter Template</h1>
+    return <>
+        <img src={IMAGE} width="300" height="100"></img>
+        <h1>React Typescript Webpack Starter Template</h1>
+    </>
 }
